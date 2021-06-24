f14edaf6c7fec19fdc3ee533a1c3f04cdeece727
Tuyet Ngo <69872401+sara-ngo@users.noreply.github.com>
2021-06-24 16:26:37 -0700

    Add files via upload

---
+++ basics-assignment-1/assignment.js
@@ -0,0 +1,10 @@
+let number = 15;
+let result;
+
+result = number + 18;
+result = result - 5;
+result = result * 3;
+result = result / 2;
+
+alert(`Initial number: ${number}`);
+alert(`Final result: ${result}`);
